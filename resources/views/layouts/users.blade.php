f89d4b6ec9b34a8d54a4b698ee68aa94c8b1a169
otitheedev <yamin@otitheesoftwaresolution>
2024-07-27 23:47:04 +0600

    git updated

--- resources/views/layouts/users.blade.php
+++ resources/views/layouts/users.blade.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -15,8 +17,9 @@
   <link rel="dns-prefetch" href="//fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-  <!-- Bootstrap CSS -->
-  @vite([ 'resources/css/app.css'])
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
   @vite([ 'resources/css/style-prefix.css'])
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
